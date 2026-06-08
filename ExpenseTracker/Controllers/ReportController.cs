846356b38e5f9012ff4ef32bb5ed3dccfc63a2ff
SUSHMITHA000 <sushmitha200429@gmail.com>
2026-06-08 12:07:59 +0530

    added category report

---
+++ ExpenseTracker/Controllers/ReportController.cs
@@ -0,0 +1,31 @@
+﻿using ExpenseTracker.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ExpenseTracker.Controllers
+{
+    public class ReportController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ReportController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult CategoryReport()
+        {
+            var report = _context.Expenses
+                .Include(e => e.Category)
+                .GroupBy(e => e.Category.CategoryName)
+                .Select(g => new
+                {
+                    Category = g.Key,
+                    TotalAmount = g.Sum(x => x.Amount)
+                })
+                .ToList();
+
+            return View(report);
+        }
+    }
+}
