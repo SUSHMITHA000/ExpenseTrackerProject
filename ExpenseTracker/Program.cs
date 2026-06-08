4950b654cab571ddeea05c5410f821bc130f8a3c
SUSHMITHA000 <sushmitha200429@gmail.com>
2026-06-08 15:18:59 +0530

    Improved login and registration page

--- ExpenseTracker/Program.cs
+++ ExpenseTracker/Program.cs
@@ -29,6 +29,7 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    //pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Account}/{action=Login}/{id?}");
 
 app.Run();
